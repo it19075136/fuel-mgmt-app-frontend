a81dbffe958104d6c6fa580768969e1e8543fa0c
pasindu <it19390260@my.sliit.lk>
2022-10-26 00:53:21 +0530

    get json object from backend

--- app/src/main/java/com/example/fuel_mgmt_app_frontend/FuelStation/FuelStationList.java
+++ app/src/main/java/com/example/fuel_mgmt_app_frontend/FuelStation/FuelStationList.java
@@ -4,15 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.fuel_mgmt_app_frontend.DBHelper;
 import com.example.fuel_mgmt_app_frontend.R;
 import com.example.fuel_mgmt_app_frontend.SelectRegisrationType;
 import com.example.fuel_mgmt_app_frontend.User.MainActivity;
 import com.google.android.material.button.MaterialButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FuelStationList extends AppCompatActivity {
 
@@ -30,10 +42,47 @@ public class FuelStationList extends AppCompatActivity {
         backIcon =  findViewById(R.id.left_icon);
         signOutIcon = findViewById(R.id.right_icon);
         DB = new DBHelper(this);
-
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
         newFuelStation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+//                String email = DB.logingEmail();
+//                Log.d("loged email", email);
+                String url = "https://fuely-api.herokuapp.com/api/fuelstation/byEmail/sandungwp@gmail.com";
+//                url += email;
+
+                JsonArrayRequest jsonArrayRequest  = new JsonArrayRequest(
+                        Request.Method.GET,
+                        url,
+                        null,
+                        new Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray response) {
+//                                res[0] = response;
+                                for (int i = 0; i < response.length(); i++) {
+
+                                    try {
+
+                                        JSONObject responseObj = response.getJSONObject(i);
+
+                                        String location = responseObj.getString("location");
+                                        String stationName = responseObj.getString("stationName");
+                                        Log.d("location", location);
+                                        Log.d("stationName", stationName);
+
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+                requestQueue.add(jsonArrayRequest);
+                JSONArray jsonArray=getStationById(url);
                 Intent intent =  new Intent(FuelStationList.this, FuelStationBasicDetails.class);
                 startActivity(intent);
             }
@@ -52,6 +101,45 @@ public class FuelStationList extends AppCompatActivity {
 
             }
         });
+
+
     }
+    public JSONArray getStationById(String url){
+         JSONArray[] res = new JSONArray[1];
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest  = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        res[0] = response;
+                        for (int i = 0; i < response.length(); i++) {
+
+                            try {
+
+                                JSONObject responseObj = response.getJSONObject(i);
+
+                                String location = responseObj.getString("location");
+                                String stationName = responseObj.getString("stationName");
+                                Log.d("location", location);
+                                Log.d("stationName", stationName);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(jsonArrayRequest);
+        return res[0];
+    }
+
 
 }
