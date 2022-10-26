59c4232f4f31a6c83715176d7f4a78956d9b36ca
it19075204 <it19075204@my.sliit.lk>
2022-10-26 21:08:46 +0530

    list show working

--- app/src/main/java/com/example/fuel_mgmt_app_frontend/FuelStation/FuelStationList.java
+++ app/src/main/java/com/example/fuel_mgmt_app_frontend/FuelStation/FuelStationList.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -34,7 +35,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 
-public class FuelStationList extends AppCompatActivity {
+public class FuelStationList extends AppCompatActivity implements RecyclerViewInterface {
 
     MaterialButton newFuelStation;
     ImageView backIcon,signOutIcon;
@@ -45,7 +46,10 @@ public class FuelStationList extends AppCompatActivity {
     ArrayList<StationModel> fuelStations = new ArrayList<StationModel>();
     MainAdapter adapter;
 
-    StationModel model  = new StationModel();
+    StationModel model;
+
+    String url = "https://fuely-api.herokuapp.com/api/fuelstation/byEmail/sandungwp@gmail.com";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,56 +66,67 @@ public class FuelStationList extends AppCompatActivity {
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
 
+
+        Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            public void run() {
+//                // yourMethod();
+//                //finish();
+//                //startActivity(getIntent());
+//            }
+//        }, 5000);
+        getFuelStationsAPI(requestQueue);
+
         newFuelStation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
                 Log.d("StartedMessage", "newFuelStation.setOnClickListener started");
 //                String email = DB.logingEmail();
 //                Log.d("loged email", email);
-                String url = "https://fuely-api.herokuapp.com/api/fuelstation/byEmail/sandungwp@gmail.com";
+     //           String url = "https://fuely-api.herokuapp.com/api/fuelstation/byEmail/sandungwp@gmail.com";
 //                url += email;
 
-                JsonArrayRequest jsonArrayRequest  = new JsonArrayRequest(
-                        Request.Method.GET,
-                        url,
-                        null,
-                        new Response.Listener<JSONArray>() {
-                            @Override
-                            public void onResponse(JSONArray response) {
-//                                res[0] = response;
-                                for (int i = 0; i < response.length(); i++) {
-
-                                    try {
-
-                                        JSONObject responseObj = response.getJSONObject(i);
-
-                                        String location = responseObj.getString("location");
-                                        String stationName = responseObj.getString("stationName");
-                                        Log.d("location", location);
-                                        Log.d("stationName", stationName);
-                                        model.setStationName(stationName);
-
-                                        fuelStations.add(model);
-
-
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                                Log.d("fuelStations Array size", String.valueOf(fuelStations.size()));
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                });
-                requestQueue.add(jsonArrayRequest);
-                JSONArray jsonArray=getStationById(url);
+//                JsonArrayRequest jsonArrayRequest  = new JsonArrayRequest(
+//                        Request.Method.GET,
+//                        url,
+//                        null,
+//                        new Response.Listener<JSONArray>() {
+//                            @Override
+//                            public void onResponse(JSONArray response) {
+////                                res[0] = response;
+//                                for (int i = 0; i < response.length(); i++) {
+//
+//                                    try {
+//
+//                                        JSONObject responseObj = response.getJSONObject(i);
+//
+//                                        String location = responseObj.getString("location");
+//                                        String stationName = responseObj.getString("stationName");
+//                                        Log.d("location", location);
+//                                        Log.d("stationName", stationName);
+//                                        model.setStationName(stationName);
+//
+//                                        fuelStations.add(model);
+//
+//
+//                                    } catch (JSONException e) {
+//                                        e.printStackTrace();
+//                                    }
+//                                }
+//                                Log.d("fuelStations Array size", String.valueOf(fuelStations.size()));
+//                            }
+//                        }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//
+//                    }
+//                });
+//                requestQueue.add(jsonArrayRequest);
+//                JSONArray jsonArray=getStationById(url);
                 Intent intent =  new Intent(FuelStationList.this, FuelStationBasicDetails.class);
                 startActivity(intent);
-
-                getFuelStationsList();
 
             }
         });
@@ -175,19 +190,82 @@ public class FuelStationList extends AppCompatActivity {
         //loop and add names  in heres
 //        model.setStationName();
         //call api before this
+        Log.d("getFlStationList size", String.valueOf(fuelStations.size()));
+
+
+        for (int i = 0; i < fuelStations.size(); i++) {
+                            Log.d("getFuelStationsList", fuelStations.get(i).getStationName());
+                        }
+
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        adapter = new MainAdapter(this,fuelStations);
+        adapter = new MainAdapter(this,fuelStations, this);
 
         recyclerView.setAdapter(adapter);
 
     }
 
+    public void getFuelStationsAPI(RequestQueue requestQueue){
+//        Toast.makeText(this,"test in",Toast.LENGTH_SHORT).show();
+
+        String url = "https://fuely-api.herokuapp.com/api/fuelstation/byEmail/sandungwp@gmail.com";
+//                url += email;
+
+        JsonArrayRequest jsonArrayRequest  = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+//                                res[0] = response;
+                        for (int i = 0; i < response.length(); i++) {
+                            model = new StationModel();
+                            try {
+
+                                JSONObject responseObj = response.getJSONObject(i);
+
+//                                String location = responseObj.getString("location");
+                                String stationName = responseObj.getString("stationName");
+//                                Log.d("location", location);
+//                                Log.d("stationName", stationName);
+                                model.setStationName(stationName);
+
+                                fuelStations.add(model);
+
+//                                Log.d("fuel station nanme", fuelStations.get(i).stationName);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        getFuelStationsList();
+
+//                        for (int i = 0; i < response.length(); i++) {
+//                            Log.d("fuelStations array loop", String.valueOf(fuelStations.get(i)));
+//                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(jsonArrayRequest);
+        JSONArray jsonArray=getStationById(url);
+
+
+    }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
 
-
+    @Override
+    public void OnItemClick(int position) {
+        Toast.makeText(this, "I've been clicked and id is = " + fuelStations.get(position).getStationName() + " & position is "+ position, Toast.LENGTH_SHORT).show();
+    }
 }
---
+++ app/src/main/java/com/example/fuel_mgmt_app_frontend/FuelStation/RecyclerViewInterface.java
@@ -0,0 +1,5 @@
+package com.example.fuel_mgmt_app_frontend.FuelStation;
+
+public interface RecyclerViewInterface {
+    void OnItemClick(int position);
+}
